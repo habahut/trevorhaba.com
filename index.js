3cfd2b2952beccbd5b8293e838fba515ca14509f
Trevor Haba <habahut@gmail.com>
2014-11-16 13:25:32 -0800

    added bower_components, makes heroku deploy easier

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require('express')
-var app = express();
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/public/main.html"));
 })
 
 app.listen(app.get('port'), function() {
