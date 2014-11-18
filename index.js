a1cbb3d17ccc472765e4794ecbeeb467fe59f9fa
Trevor Haba <habahut@gmail.com>
2014-11-17 22:08:42 -0800

    asdf

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.resolve(__dirname + "/public/main.html"));
 })
 
+app.get('/parallax', function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/public/parallax.html"));
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
